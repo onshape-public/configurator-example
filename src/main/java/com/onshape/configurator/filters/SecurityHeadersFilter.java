31327f97cda21f9dcf123500c607a98be924a01b
Paritosh Bhide <pbhide@ptc.com>
2026-01-20 11:07:32 -0500

    BEL-211272: Added a security filter to only allow *.onshape.com websites to embed this application

---
+++ src/main/java/com/onshape/configurator/filters/SecurityHeadersFilter.java
@@ -0,0 +1,68 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 Onshape Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.onshape.configurator.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class SecurityHeadersFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // No initialization needed
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        // Content-Security-Policy frame-ancestors is the modern replacement for X-Frame-Options
+        // It allows wildcard subdomains and multiple domains
+        httpResponse.setHeader("Content-Security-Policy", "frame-ancestors 'self' https://*.onshape.com");
+        
+        // X-Frame-Options as fallback for older browsers (does not support wildcards)
+        // Note: ALLOW-FROM is deprecated and not supported in modern browsers, but some legacy browsers still use it
+        // For better compatibility with wildcard domains, Content-Security-Policy is preferred
+        httpResponse.setHeader("X-Frame-Options", "ALLOW-FROM https://onshape.com");
+        
+        // Additional security headers
+        httpResponse.setHeader("X-Content-Type-Options", "nosniff");
+        httpResponse.setHeader("X-XSS-Protection", "1; mode=block");
+        
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        // No cleanup needed
+    }
+}
